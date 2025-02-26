edc560df7f5707faca6ca4c23758e5c5566782d1
Dragonstardust7 <ivannevolin2007@mail.ru>
2025-02-26 18:27:57 +0500

    Убрано подключение к бд

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,20 @@
-import express from 'express';
-import 'dotenv/config';
-import pg from 'pg';
-
-const { Client } = pg;
-const client = new Client({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT || 'Ошибка при получении порта.'),
-});
+import express from "express";
+import "dotenv/config";
 
 async function main() {
-  await client.connect();
-
   const app = express();
   const dbPort = process.env.PORT;
 
   app.use(express.json());
-
-  app.get('/', (req, res) => {
-    res.send('Привет Мага!');
+  app.use(express.urlencoded({ extended: true }));
+  
+  app.get("/", (req, res) => {
+    res.send("Привет Мага!");
   });
 
   app.listen(dbPort, () => {
     console.log(`Сервер запущен на http://127.0.0.1:${dbPort}`);
   });
-
-  const result = await client.query('SELECT \$1::text as message', ['Hello world!']);
-  console.log(result.rows[0].message);
-  await client.end();
 }
 
 main();
