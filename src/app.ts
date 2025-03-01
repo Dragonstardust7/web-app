91129c246ed5f9e6e3088423537e345447a70d7c
Dragonstardust7 <ivannevolin2007@mail.ru>
2025-03-01 21:36:10 +0500

    Добавлена таблица

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import "dotenv/config";
+import {Pool} from "pg";
 
   const app = express();
   const dbPort = process.env.PORT;
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: Number(process.env.PGPORT),
+});
 
   app.get("/", (req, res) => {
     res.send("Привет Мага!");
