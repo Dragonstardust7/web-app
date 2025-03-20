a488a34c6920c02775f2ff6006a8e75a0f33e493
Dragonstardust7 <ivannevolin2007@mail.ru>
2025-03-20 18:13:39 +0500

    Исправлено название роутера, использовать +port!. Также вместо pool использован client

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import "dotenv/config";
-import UserRouter from './users/users';
+import userRouter from './users/users';
 
 const app = express();
 const port = process.env.PORT;
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/users', UserRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://127.0.0.1:${port}`);
